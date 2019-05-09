48627ed45ff359ac8b12015a1e208d70032bf563
Samuel Stenton <5670870+SamStenton@users.noreply.github.com>
2019-05-09 19:10:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('yeah soz'))
+app.get('/', (req, res) => res.send('Go build something amazing!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
