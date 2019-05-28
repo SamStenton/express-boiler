e68c7e8ed41b1a58ddf8296c9b040e3307cd050b
Samuel Stenton <5670870+SamStenton@users.noreply.github.com>
2019-05-28 19:17:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = env.NODE_PORT || 3000
 
 app.get('/', (req, res) => res.send('Go build something amazing!'))
 
